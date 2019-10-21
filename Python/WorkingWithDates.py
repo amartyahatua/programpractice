0116e298227987d296f35f1ca79652ccd8fb17c2
Amartya Hatua <amartyahatua@gmail.com>
2019-10-21 17:22:33 -0500

    Python practice for date and time

---
+++ Python/WorkingWithDates.py
@@ -0,0 +1,7 @@
+# How many days do I have util my birthday?
+# When is my poject due?
+# I want to book an appointment in two weeks, what will the date be?
+import  datetime
+currentdate = datetime.date.today()
+
+print(currentdate)
