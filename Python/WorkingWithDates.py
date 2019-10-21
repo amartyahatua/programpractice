2c7cac739db929b9a491d6879fd27fdc50807e16
Amartya Hatua <amartyahatua@gmail.com>
2019-10-21 17:55:56 -0500

    Python practice for date and time

--- Python/WorkingWithDates.py
+++ Python/WorkingWithDates.py
@@ -3,5 +3,25 @@
 # I want to book an appointment in two weeks, what will the date be?
 import  datetime
 currentdate = datetime.date.today()
-
 print(currentdate)
+
+print(currentdate.day)
+print(currentdate.month)
+print(currentdate.year)
+
+# Year with four digits
+print(currentdate.strftime('%d %b,%Y'))
+
+# Year with two digits
+print(currentdate.strftime('%d %b,%y'))
+
+print(currentdate.strftime('Please attend our event %A %B %d in the year %Y'))
+
+
+userInput = input('Please enter your birthday (mm/dd/yyyy)')
+birthday = datetime.datetime.strptime(userInput,'%m/%d/%Y')
+print(birthday)
+
+currentdate = datetime.datetime.today()
+difference =  currentdate - birthday
+print(difference.days)
