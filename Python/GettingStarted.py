3c0d8f30a4d020c061a4a346842d1a394d46003e
Amartya Hatua <amartyahatua@gmail.com>
2019-10-21 12:42:23 -0500

    Python practice

---
+++ Python/GettingStarted.py
@@ -0,0 +1,31 @@
+#Source https://www.youtube.com/watch?v=3cZsjOclmoM
+#This is a comment
+#And I can add lots if comments
+
+print("Hello world")
+
+
+## We can use single or double quotes
+print('The capybera is the world largest rodent')
+print("The capybera likes live in groups")
+print("The capybera can swim")
+
+
+## The problem may occur when there is a single quote inside the sentence like "It's a beautiful day". In this case, if we use
+## sigle quote then the program may understand the single quote is ending after "It". In such cases we need to use double quotes.
+## On the other hand if there is a double quotes inside my string, we can use a single string to quote the entire string
+
+print("It's a beautiful day")
+print('He said "have a look"')
+
+## use \n forcefully introducing a new line
+## \t for a tab
+
+print("Hellow how are you? \nIt is raining today.")
+
+
+## Triple quotes
+## It allows you to get the output the way you have written in the print statement. It also allows the new lines.
+print("""Hello 
+how are you? 
+I am in Mississippi""")
