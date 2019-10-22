0723bcdbdc3714038dd096adc74312d80722562a
Amartya Hatua <amartyahatua@gmail.com>
2019-10-22 17:56:07 -0500

    Conditional statement

---
+++ Python/ConditionalStatement.py
@@ -0,0 +1,17 @@
+answer = input("Would you like express shipping \n")
+
+## Converting all the answers in lower case so that we can accept any input
+answer = answer.lower()
+if (answer == "yes"):
+    print("That will be an extra $10")
+
+print("Have a nice day")
+
+favouriTeam = input("What is your favourite team \n ")
+favouriTeam = favouriTeam.lower()
+
+if(favouriTeam == "senators"):
+    print("Yeah go Sens go!!")
+    print("But we miss our captain")
+
+print("Its ok if you prefer Football/Soccer")
